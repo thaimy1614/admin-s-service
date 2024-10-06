c477798da55dcce0d676738fbcc21254731ddd09
quocthai <lxlthailxl@gmail.com>
2024-10-07 01:12:39 +0700

    init admin page

---
+++ src/layouts/config-nav-dashboard.tsx
@@ -0,0 +1,37 @@
+import { Label } from 'src/components/label';
+import { SvgColor } from 'src/components/svg-color';
+
+// ----------------------------------------------------------------------
+
+const icon = (name: string) => (
+  <SvgColor width="100%" height="100%" src={`/assets/icons/navbar/${name}.svg`} />
+);
+
+export const navData = [
+  {
+    title: 'Dashboard',
+    path: '/',
+    icon: icon('ic-analytics'),
+  },
+  {
+    title: 'User',
+    path: '/user',
+    icon: icon('ic-user'),
+  },
+  {
+    title: 'User',
+    path: '/user',
+    icon: icon('ic-user'),
+  },
+  {
+    title: 'User',
+    path: '/user',
+    icon: icon('ic-user'),
+  },
+  {
+    title: 'User',
+    path: '/user',
+    icon: icon('ic-user'),
+  },
+  
+];
