ecc14c05e197afb7098a2a76b1d3f3e69984886d
quocthai <lxlthailxl@gmail.com>
2024-10-09 13:56:37 +0700

    implement update and delete user

--- src/sections/user/user-table-row.tsx
+++ src/sections/user/user-table-row.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import { useState, useCallback } from 'react';
 
 import Box from '@mui/material/Box';
@@ -25,12 +27,14 @@ export type UserProps = {
 };
 
 type UserTableRowProps = {
+  handleUpdate: Function;
+  handleDelete: Function;
   row: UserProps;
   selected: boolean;
   onSelectRow: () => void;
 };
 
-export function UserTableRow({ row, selected, onSelectRow }: UserTableRowProps) {
+export function UserTableRow({handleUpdate, handleDelete, row, selected, onSelectRow }: UserTableRowProps) {
   const [openPopover, setOpenPopover] = useState<HTMLButtonElement | null>(null);
 
   const handleOpenPopover = useCallback((event: React.MouseEvent<HTMLButtonElement>) => {
@@ -96,12 +100,12 @@ export function UserTableRow({ row, selected, onSelectRow }: UserTableRowProps) 
             },
           }}
         >
-          <MenuItem onClick={handleClosePopover}>
+          <MenuItem onClick={() => handleUpdate(row.id)}>
             <Iconify icon="solar:pen-bold" />
             Chỉnh sửa
           </MenuItem>
 
-          <MenuItem onClick={handleClosePopover} sx={{ color: 'error.main' }}>
+          <MenuItem onClick={() => handleDelete(row.id)} sx={{ color: 'error.main' }}>
             <Iconify icon="solar:trash-bin-trash-bold" />
             Xóa
           </MenuItem>
--- src/sections/user/view/user-view.tsx
+++ src/sections/user/view/user-view.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect, SetStateAction } from 'react';
 
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -25,6 +25,8 @@ import { emptyRows, applyFilter, getComparator } from '../utils';
 import type { UserProps } from '../user-table-row';
 import { getToken } from 'src/services/localStorageService';
 import { useNavigate } from 'react-router-dom';
+import FormModal from 'src/components/common/form-modal';
+import MessageModal from 'src/components/common/message-modal';
 
 // ----------------------------------------------------------------------
 
@@ -34,14 +36,24 @@ export function UserView() {
   const table = useTable();
 
   const [filterName, setFilterName] = useState('');
-  const [users, setUsers] = useState([{
-    id: '',
-    name: '',
-    email: '',
-    address: '',
-    phone: '',
-    status: 'ACTIVE',
-  }]);
+  const [users, setUsers] = useState([
+    {
+      id: '',
+      name: '',
+      email: '',
+      address: '',
+      phone: '',
+      status: 'ACTIVE',
+    },
+  ]);
+  const [isFormModalOpen, setIsFormModalOpen] = useState(false);
+  const [messageModalOpen, setMessageModal] = useState(false);
+  const [messageType, setMessageType] = useState(false);
+  const [message, setMessage] = useState('Đổi thông tin thành công!');
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState('');
+  const [userId, setUserId] = useState('');
 
   const dataFiltered: UserProps[] = applyFilter({
     inputData: users,
@@ -68,14 +80,147 @@ export function UserView() {
     }
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     if (!getToken()) {
       navigate('/sign-in');
     }
 
     fetchUsers();
-
   }, [navigate]);
+
+  const handleFormModalClose = () => {
+    setIsFormModalOpen(false);
+    setMessageModal(false);
+  };
+
+  const handleChangeUserInfo = (id: any) => {
+    console.log(id);
+    fetch(import.meta.env.VITE_APP_API + '/admin/user/' + id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json', // Set the content type to JSON
+      },
+      body: JSON.stringify({
+        name: name,
+        phone: phone,
+        address: address,
+      }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log('Response body:', data);
+        if (data.result) {
+          setUsers((prevUsers) =>
+            prevUsers.map((user) => (user.id === id ? { ...user, name, phone, address } : user))
+          );
+          setIsFormModalOpen(false);
+          setMessageType(true);
+          setMessage('Chỉnh sửa thông tin thành công!');
+          setMessageModal(true);
+          return;
+        } else {
+          setIsFormModalOpen(true);
+          setMessageType(false);
+          setMessage('Vui lòng nhập chính xác!');
+          setMessageModal(true);
+        }
+      })
+      .catch((error) => {
+        setIsFormModalOpen(false);
+        setMessageType(false);
+        setMessage('Đã có lỗi xảy ra, vui lòng thử lại!');
+        setMessageModal(true);
+        console.log(error);
+      });
+  };
+
+  const checkOTPFormData = (id: any) => {
+    return {
+      title: 'Chỉnh sửa thông tin khách hàng',
+      fields: [
+        {
+          value: name,
+          label: 'Tên',
+          name: 'name',
+          required: true,
+          onChange: (e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value),
+          type: 'text',
+        },
+        {
+          value: phone,
+          label: 'Số điện thoại',
+          name: 'phone',
+          type: 'text',
+          required: true,
+          onChange: (e: React.ChangeEvent<HTMLInputElement>) => setPhone(e.target.value),
+        },
+        {
+          value: address,
+          label: 'Địa chỉ',
+          name: 'address',
+          type: 'text',
+          required: true,
+          onChange: (e: React.ChangeEvent<HTMLInputElement>) => setAddress(e.target.value),
+        },
+      ],
+      submitText: 'Chỉnh sửa',
+    };
+  };
+
+  const handleMessageModalClose = () => {
+    setMessageModal(false);
+  };
+
+  const handleDelete = (id: any) => {
+    console.log(id);
+    fetch(import.meta.env.VITE_APP_API + '/admin/user/' + id, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json', // Set the content type to JSON
+      },
+      body: JSON.stringify({
+        name: name,
+        phone: phone,
+        address: address,
+      }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log('Response body:', data);
+        if (data.result===true) {
+          const status = "DELETED"
+          setUsers((prevUsers) =>
+            prevUsers.map((user) => (user.id === id ? { ...user, status,  } : user))
+          );
+          setIsFormModalOpen(false);
+          setMessageType(true);
+          setMessage('Xóa tài khoản khách hàng thành công!');
+          setMessageModal(true);
+          return;
+        }
+      })
+      .catch((error) => {
+        setIsFormModalOpen(false);
+        setMessageType(false);
+        setMessage('Đã có lỗi xảy ra, vui lòng thử lại!');
+        setMessageModal(true);
+        console.log(error);
+      });
+  };
+
+  const handleShowUpdateForm = (id: any) => {
+    const user = users.find((user) => user.id === id);
+    setName(user?.name || '');
+    setPhone(user?.phone || '');
+    setAddress(user?.address || '');
+    setUserId(id);
+    setIsFormModalOpen(true);
+    setMessageModal(false);
+  };
 
   return (
     <DashboardContent>
@@ -135,6 +280,8 @@ export function UserView() {
                   )
                   .map((row) => (
                     <UserTableRow
+                      handleUpdate={handleShowUpdateForm}
+                      handleDelete={handleDelete}
                       key={row.id}
                       row={row}
                       selected={table.selected.includes(row.id)}
@@ -163,6 +310,22 @@ export function UserView() {
           onRowsPerPageChange={table.onChangeRowsPerPage}
         />
       </Card>
+      {messageModalOpen && (
+        <MessageModal
+          message={message}
+          open={messageModalOpen}
+          handleClose={handleMessageModalClose}
+          messageType={messageType}
+        />
+      )}
+      {isFormModalOpen && (
+        <FormModal
+          handleClose={handleFormModalClose}
+          open={isFormModalOpen}
+          formData={checkOTPFormData(userId)}
+          onSubmit={() => handleChangeUserInfo(userId)}
+        />
+      )}
     </DashboardContent>
   );
 }
